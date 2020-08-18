10084fb3b14b8706252d8417a6c562e15453640d
LoniQin <354060093@qq.com>
2020-08-19 01:14:17 +0800

    Add User Defaults Extension.

---
+++ Sources/FoundationLib/extensions/UserDefaults+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UserDefaults+Extension.swift
+//
+//
+//  Created by lonnie on 2020/8/19.
+//
+
+import Foundation
+
+public extension UserDefaults {
+    
+    func get<T>(_ key: CustomStringConvertible) -> T? {
+        return value(forKey: key.description) as? T
+    }
+    
+    func set(_ any: Any, for key: CustomStringConvertible) {
+        set(any, forKey: key.description)
+    }
+    
+}
--- Tests/FoundationLibTests/FoundationLibTests.swift
+++ Tests/FoundationLibTests/FoundationLibTests.swift
@@ -2,14 +2,14 @@ import XCTest
 @testable import FoundationLib
 
 final class FoundationLibTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(FoundationLib().text, "Hello, World!")
+    
+    func testUserDefaults() {
+        UserDefaults.standard.set(1, for: 2)
+        XCTAssert(UserDefaults.standard.get(2) == 1)
+        UserDefaults.standard.set(5, for: "333")
+        XCTAssert(UserDefaults.standard.get("333") == 5)
     }
-
     static var allTests = [
-        ("testExample", testExample),
+        ("testUserDefaults", testUserDefaults),
     ]
 }
