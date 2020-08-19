a862eddeb8bf7881b1841170b0b30e807fdaf439
LoniQin <longqinlonger@hotmail.com>
2020-08-19 23:23:30 +0800

    Add string extension.

--- Sources/FoundationLib/extensions/String+Extension.swift
+++ Sources/FoundationLib/extensions/String+Extension.swift
@@ -5,6 +5,26 @@
 //  Created by lonnie on 2020/8/19.
 //
 
+public extension String {
+    
+    func appendingPrefix(_ string: String) -> String {
+        if self.hasPrefix(string) {
+            return self
+        } else {
+            return string + self
+        }
+    }
+
+    func appendingSuffix(_ string: String) -> String {
+        if self.hasSuffix(string) {
+            return self
+        } else {
+            return self + string
+        }
+    }
+    
+}
+
 public func / (lhs: String, rhs: String) -> String {
     "\(lhs)/\(rhs)"
 }
