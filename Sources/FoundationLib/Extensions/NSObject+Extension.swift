e644beb3a27c75b93706f6fa8d166843413accfb
LoniQin <longqinlonger@hotmail.com>
2020-08-20 21:19:23 +0800

    Optimize code.

---
+++ Sources/FoundationLib/Extensions/NSObject+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  NSObject+Extension.swift
+//
+//
+//  Created by lonnie on 2020/8/19.
+//
+
+import Foundation
+
+public extension NSObject {
+    
+    public func setAssociatedValue(_ value: Any?, with key: inout String, _ option: objc_AssociationPolicy = .OBJC_ASSOCIATION_COPY) {
+         objc_setAssociatedObject(self, &key, value, option)
+     }
+     
+    public func getAssociatedValue<T>(with key: inout String) -> T? {
+         objc_getAssociatedObject(self, &key) as? T
+    }
+}
