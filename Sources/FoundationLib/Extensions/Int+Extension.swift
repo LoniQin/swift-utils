29abce0f0dc4dbda53cd9bee2ddd927ee0e25005
LoniQin <longqinlonger@hotmail.com>
2020-08-23 01:23:58 +0800

    Add more functions to Int Extension.

--- Sources/FoundationLib/Extensions/Int+Extension.swift
+++ Sources/FoundationLib/Extensions/Int+Extension.swift
@@ -6,6 +6,36 @@
 //
 
 import Foundation
+
+public extension Int {
+    
+    func toBinaryString() -> String {
+        var s = [Int]()
+        var n = self
+        while n > 0 {
+            s.append(n % 2)
+            n = n / 2
+        }
+        var text = ""
+        while let value = s.popLast() {
+            text.append(value.description)
+        }
+        return text
+    }
+    
+    @discardableResult
+    mutating func decreasing() -> Int {
+        self -= 1
+        return self
+    }
+    
+    @discardableResult
+    mutating func increasing() -> Int {
+        self += 1
+        return self
+    }
+}
+
 extension Int: NumberConvertable {
     
     public var int: Int { self  }
