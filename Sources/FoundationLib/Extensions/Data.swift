7d65688dfbdc63cbfe944236586a9111cb13e8ec
LoniQin <longqinlonger@hotmail.com>
2020-09-13 13:13:07 +0800

    Optimize code.

--- Sources/FoundationLib/Extensions/Data.swift
+++ Sources/FoundationLib/Extensions/Data.swift
@@ -7,52 +7,56 @@
 
 import Foundation
 
+let charA = UInt8(UnicodeScalar("a").value)
+let char0 = UInt8(UnicodeScalar("0").value)
+
+private func itoh(_ value: UInt8) -> UInt8 {
+    return (value > 9) ? (charA + value - 10) : (char0 + value)
+}
+
+private func htoi(_ value: UInt8) throws -> UInt8 {
+    switch value {
+    case char0...char0 + 9:
+        return value - char0
+    case charA...charA + 5:
+        return value - charA + 10
+    default:
+        throw CryptoError.codingError
+    }
+}
+
 extension Data {
     
     init(hex: String) throws {
         self.init()
-        var buffer: UInt8?
-        var skip = hex.hasPrefix("0x") ? 2 : 0
-        for char in hex.unicodeScalars.lazy {
-            guard skip == 0 else {
-                skip -= 1
-                continue
-            }
-            guard char.value >= 48 && char.value <= 102 else {
-                throw CryptoError.codingError
-            }
-            let v: UInt8
-            let c: UInt8 = UInt8(char.value)
-            switch c {
-            case let c where c <= 57:
-                v = c - 48
-            case let c where c >= 65 && c <= 70:
-                v = c - 55
-            case let c where c >= 97:
-                v = c - 87
-            default:
-                throw CryptoError.codingError
-            }
-            if let b = buffer {
-                append(b << 4 | v)
-                buffer = nil
-            } else {
-                buffer = v
-            }
+
+        if hex.count % 2 != 0 || hex.count == 0 {
+            throw CryptoError.codingError
         }
-        if let b = buffer {
-            append(b)
+
+        let stringBytes: [UInt8] = Array(try hex.data(.utf8))
+
+        for i in 0...((hex.count / 2) - 1) {
+            let char1 = stringBytes[2 * i]
+            let char2 = stringBytes[2 * i + 1]
+            try self.append(htoi(char1) << 4 + htoi(char2))
         }
     }
     
     var hex: String {
-        return `lazy`.reduce("") {
-            var s = String($1, radix: 16)
-            if s.count == 1 {
-                s = "0" + s
+        let hexLen = self.count * 2
+        let ptr = UnsafeMutablePointer<UInt8>.allocate(capacity: hexLen)
+        var offset = 0
+        
+        self.regions.forEach { (_) in
+            for i in self {
+                ptr[Int(offset * 2)] = itoh((i >> 4) & 0xF)
+                ptr[Int(offset * 2 + 1)] = itoh(i & 0xF)
+                offset += 1
             }
-            return $0 + s
         }
+        
+        return String(bytesNoCopy: ptr, length: hexLen, encoding: .utf8, freeWhenDone: true)!
     }
     
     func string(_ encoding: Encoding) throws -> String {
