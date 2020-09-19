150d49701e6b95a49c8c69e0e0428dab4043138c
LoniQin <longqinlonger@hotmail.com>
2020-09-19 16:09:10 +0800

    optimize code.

--- Tests/FoundationLibTests/functions.swift
+++ Tests/FoundationLibTests/functions.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 
-func dataPath() -> String {
-    var comp = #file.components(separatedBy: "/")
+func dataPath(_ file: String = #file) -> String {
+    var comp = file.components(separatedBy: "/")
     comp.removeLast()
     comp.append("data")
     return comp.joined(separator: "/")
