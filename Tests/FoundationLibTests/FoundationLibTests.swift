e107664d4ee3df5822d991f04dc45082493ea7c0
LoniQin <longqinlonger@hotmail.com>
2020-10-05 15:37:47 +0800

    Add more unit tests.

--- Tests/FoundationLibTests/FoundationLibTests.swift
+++ Tests/FoundationLibTests/FoundationLibTests.swift
@@ -346,7 +346,7 @@ final class FoundationLibTests: XCTestCase {
         }
     }
     
-    func testDynamicObject() {
+    func testDynamicObject() throws {
         let dog = DynamicObject.new(name: "Lily", age: 12)
         dog.name.assert.equal("Lily")
         dog.age.assert.equal(12)
@@ -355,9 +355,30 @@ final class FoundationLibTests: XCTestCase {
         }
         dog.increaseAge()
         dog.age.assert.equal(13)
+        let sequence = (0..<1000000).map({$0})
+        for i in sequence {
+            dog.dynamicallyCall(withKeywordArguments: KeyValuePairs(dictionaryLiteral: (i.description, i)))
+        }
+        try DebugLogger.default.measure(desc: "Test access integer using DynamicObject") {
+            var value: Int = 0
+            for _ in 0..<1000000 {
+                value = dog.100000
+            }
+        }
+        try DebugLogger.default.measure(desc: "Test access integer using Dictionary") {
+            var value: Int = 0
+            for i in 0..<1000000 {
+                value = dog.params[String(i)] as! Int
+            }
+        }
+        try DebugLogger.default.measure(desc: "Test access integer using Array") {
+            for i in 0..<1000000 {
+                sequence[i]
+            }
+        }
     }
     
-    func testMathObject() {
+    func testMathObject() throws {
         
         class Math: DynamicObject {
             
@@ -396,12 +417,35 @@ final class FoundationLibTests: XCTestCase {
         math.negate = {
             -$0
         }
+        math.sin = {
+            sin($0)
+        }
+        math.cos = {
+            cos($0)
+        }
+        math.tan = {
+            tan($0)
+        }
         math.add(2, 3).assert.equal(5)
         math.minus(11, 4).assert.equal(7)
         math.multiply(33, 3).assert.equal(99)
         math.divide(44, 4).assert.equal(11)
         math.negate(5).assert.equal(-5)
-        
+        let sequence = (0..<10000000).map(Double.init)
+        try DebugLogger.default.measure(desc: "Caulcate with dynamic object") {
+            autoreleasepool {
+                sequence.forEach {
+                    math.sin($0)
+                }
+            }
+        }
+        try DebugLogger.default.measure(desc: "Caulcate with original function") {
+            autoreleasepool {
+                sequence.forEach {
+                    sin($0)
+                }
+            }
+        }
     }
     
     static var allTests = [
