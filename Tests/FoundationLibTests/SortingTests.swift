1d00e16ed87b67d41af2458c4b823c88213b878d
LoniQin <longqinlonger@hotmail.com>
2020-10-12 08:26:09 +0800

    Optimize unit tests.

--- Tests/FoundationLibTests/SortingTests.swift
+++ Tests/FoundationLibTests/SortingTests.swift
@@ -13,10 +13,14 @@ import XCTest
 final class SortingTests: XCTestCase {
     
     func testSorting() throws {
-        let array = (1..<1000000).map { $0 }
+        var array = [Int]()
+        for i in 0..<100 {
+            for _ in 0..<100 {
+                array.append(i)
+            }
+        }
         let shuffled = array.shuffled()
-        
-        try [SortingAlgorithm.native, SortingAlgorithm.quick, SortingAlgorithm.quick3Way, SortingAlgorithm.heap].forEach { algorithm in
+        try SortingAlgorithm.allCases.forEach { algorithm in
             var arr = shuffled
             try DebugLogger.default.measure(desc: "Test \(algorithm) sorting algorithm") {
                 arr.sort(algorithm: algorithm, comparator: <)
@@ -25,15 +29,11 @@ final class SortingTests: XCTestCase {
         }
     }
     
-    func testSortingWithDuplicated() throws {
-        var array = [Int]()
-        for i in 0..<100 {
-            for _ in 0..<100 {
-                array.append(i)
-            }
-        }
+    func testSortingLargeArray() throws {
+        let array = (1..<1000000).map { $0 }
         let shuffled = array.shuffled()
-        try [SortingAlgorithm.native, SortingAlgorithm.quick, SortingAlgorithm.quick3Way, SortingAlgorithm.shell].forEach { algorithm in
+        
+        try [SortingAlgorithm.native, SortingAlgorithm.quick, SortingAlgorithm.quick3Way, SortingAlgorithm.heap].forEach { algorithm in
             var arr = shuffled
             try DebugLogger.default.measure(desc: "Test \(algorithm) sorting algorithm") {
                 arr.sort(algorithm: algorithm, comparator: <)
