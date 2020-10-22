5e016733e7d0fc3bcf7617d534300bf2c865b588
LoniQin <longqinlonger@hotmail.com>
2020-10-22 21:50:56 +0800

    Update unit test.

--- Tests/FoundationLibTests/Base/PropertyWrappers/AssociatedPropertyTestCase.swift
+++ Tests/FoundationLibTests/Base/PropertyWrappers/AssociatedPropertyTestCase.swift
@@ -12,7 +12,36 @@ import XCTest
 
 final class AssociatedPropertyTestCase: XCTestCase {
     
-    func testSample() {
+    func testAssociatedProperty() {
+        
+        class Dog {
+            @AssociatedProperty var name: String!
+            @AssociatedProperty var age: Int!
+            @AssociatedProperty var greet: (()->Void)?
+            
+        }
+        
+        let dog = Dog()
+        dog.name = "Tom"
+        var text = ""
+        dog.greet = {
+            print("Hello", terminator: "", to: &text)
+        }
+        dog.greet?()
+        text.assert.equal("Hello")
+        text = ""
+        dog.greet = {
+            print("Hello world", terminator: "", to: &text)
+        }
+        dog.greet?()
+        text.assert.equal("Hello world")
+        dog.age = 1
+        dog.name.assert.equal("Tom")
+        dog.age.assert.equal(1)
+        dog.name = "Jack"
+        dog.age = 2
+        dog.name.assert.equal("Jack")
+        dog.age.assert.equal(2)
     
     }
     
