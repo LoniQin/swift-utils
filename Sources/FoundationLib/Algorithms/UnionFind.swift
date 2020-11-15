e299fbdd98d0b25a5e2e00afacc7f7d7351c63be
LoniQin <longqinlonger@hotmail.com>
2020-11-15 20:19:49 +0800

    Add union find algorithm.

--- Sources/FoundationLib/Algorithms/UnionFind.swift
+++ Sources/FoundationLib/Algorithms/UnionFind.swift
@@ -6,29 +6,41 @@
 //
 
 import Foundation
-public class UnionFind {
+// Weighted quick-union by rank with path compression
+class UnionFind {
+    var parent: [Int]
+    var rank: [Int]
+    var count: Int
     
-    private var id: [Int]
-    
-    public var count: Int
-    
-    public init(_ count: Int) {
+    init(_ count: Int) {
         self.count = count
-        id = (0..<count).map { $0 }
+        parent = (0..<count).map { $0 }
+        rank = .init(repeating: 0, count: count)
     }
     
-    public func connected(_ p: Int, _ q: Int) -> Bool {
-        id[p] == id[q]
-    }
-    
-    public func union(_ p: Int, _ q: Int) {
-        let pid = id[p]
-        let qid = id[q]
-        for i in 0..<id.count {
-            if id[i] == pid {
-                id[i] = qid
-            }
+    func find(_ p: Int) -> Int {
+        var p = p
+        while p != parent[p] {
+            parent[p] = parent[parent[p]]
+            p = parent[p]
         }
+        return p
     }
     
+    func connected(_ p: Int, _ q: Int) -> Bool {
+        find(p) == find(q)
+    }
+    
+    func union(_ p: Int, _ q: Int) {
+        let rootP = find(p)
+        let rootQ = find(q)
+        if rootP == rootQ { return }
+        if rank[rootP] < rank[rootQ] { parent[rootP] = rootQ }
+        else if rank[rootP] > rank[rootQ] { parent[rootQ] = rootP }
+        else {
+            parent[rootQ] = rootP
+            rank[rootP] += 1
+        }
+        count -= 1
+    }
 }
