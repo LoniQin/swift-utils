76916b412102192196c04253246f83395fa846ad
Lonnie <longqinlonger@hotmail.com>
2020-12-14 16:43:52 +0800

    Add dictoinary

--- Sources/FoundationLib/Protocols/Convertables/ResponseConvertable.swift
+++ Sources/FoundationLib/Protocols/Convertables/ResponseConvertable.swift
@@ -32,6 +32,17 @@ extension Data: ResponseConvertable {
     
 }
 
+extension Dictionary: ResponseConvertable where Key == String, Value == Any {
+    
+    public static func toResponse(with data: Data) throws -> Dictionary<String, Any> {
+        if let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] {
+            return dictionary
+        }
+        throw FoundationError.invalidCoding
+    }
+    
+}
+
 #if canImport(UIKit)
 
 import UIKit
