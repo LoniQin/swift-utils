a11173e389f072af7e555f47c644c066ce014698
Lonnie <longqinlonger@hotmail.com>
2021-01-06 23:10:28 +0800

    Code refactor.

--- Sources/FoundationLib/Crypto/MD5.swift
+++ Sources/FoundationLib/Crypto/MD5.swift
@@ -17,153 +17,8 @@ fileprivate extension UInt32 {
 public struct MD5 {
     
     public static let blockSize = 16
-    
-    private var data: [UInt8] = .init(repeating: 0, count: 64)
-    
-    private var dataLength = 0
-    
-    private var bitLength = 0
-    
-    private var state: [UInt32] = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476]
-    
     public init() {
 
-    }
-    
-    private func ROTLEFT(_ a: UInt32, _ b: UInt32) -> UInt32 {
-        ((a << b) | (a >> (32 - b)))
-    }
-
-    private func F(_ x: UInt32, _ y: UInt32, _ z: UInt32) -> UInt32 {
-        (x & y) | (~x & z)
-    }
-
-    private func G(_ x: UInt32, _ y: UInt32, _ z: UInt32) -> UInt32 {
-        (x & z) | (y & ~z)
-    }
-
-    private func H(_ x: UInt32, _ y: UInt32, _ z: UInt32) -> UInt32 {
-        x ^ y ^ z
-    }
-
-    private func I(_ x: UInt32, _ y: UInt32, _ z: UInt32) -> UInt32 {
-        y ^ (x | ~z)
-    }
-
-    private func sum(_ items: UInt32...) -> UInt32 {
-        var total: UInt32 = 0
-        for item in items {
-            total = total.addingReportingOverflow(item).partialValue
-        }
-        return total
-    }
-
-    fileprivate func FF(_ a: inout UInt32, _ b: UInt32, _ c: UInt32, _ d: UInt32, _ m: UInt32, _ s: UInt32, _ t: UInt32) {
-        a = sum(a, F(b, c, d), m, t)
-        a = sum(b, ROTLEFT(a, s))
-    }
-
-    fileprivate func GG(_ a: inout UInt32, _ b: UInt32, _ c: UInt32, _ d: UInt32, _ m: UInt32, _ s: UInt32, _ t: UInt32) {
-        a = sum(a, G(b, c, d), m, t)
-        a = sum(b, ROTLEFT(a, s))
-    }
-
-    fileprivate func HH(_ a: inout UInt32, _ b: UInt32, _ c: UInt32, _ d: UInt32, _ m: UInt32, _ s: UInt32, _ t: UInt32) {
-        a = sum(a, H(b, c, d), m, t)
-        a = sum(b, ROTLEFT(a, s))
-    }
-
-    fileprivate func II(_ a: inout UInt32, _ b: UInt32, _ c: UInt32, _ d: UInt32, _ m: UInt32, _ s: UInt32, _ t: UInt32) {
-        a = sum(a, I(b, c, d), m, t)
-        a = sum(b, ROTLEFT(a, s))
-    }
-    
-    private mutating func transform() {
-        var m = [UInt32](repeating: 0, count: Self.blockSize)
-        var j = 0
-        for i in 0..<Self.blockSize {
-            m[i] = UInt32(data[j]) + (UInt32(data[j + 1]) << 8) + (UInt32(data[j + 2]) << 16) + (UInt32(data[j + 3]) << 24)
-            j += 4
-        }
-        
-        var a = state[0], b = state[1], c = state[2], d = state[3]
-
-        // Round 1
-        FF(&a,b,c,d,m[0],  7,0xd76aa478)
-        FF(&d,a,b,c,m[1], 12,0xe8c7b756)
-        FF(&c,d,a,b,m[2], 17,0x242070db)
-        FF(&b,c,d,a,m[3], 22,0xc1bdceee)
-        FF(&a,b,c,d,m[4],  7,0xf57c0faf)
-        FF(&d,a,b,c,m[5], 12,0x4787c62a)
-        FF(&c,d,a,b,m[6], 17,0xa8304613)
-        FF(&b,c,d,a,m[7], 22,0xfd469501)
-        FF(&a,b,c,d,m[8],  7,0x698098d8)
-        FF(&d,a,b,c,m[9], 12,0x8b44f7af)
-        FF(&c,d,a,b,m[10],17,0xffff5bb1)
-        FF(&b,c,d,a,m[11],22,0x895cd7be)
-        FF(&a,b,c,d,m[12], 7,0x6b901122)
-        FF(&d,a,b,c,m[13],12,0xfd987193)
-        FF(&c,d,a,b,m[14],17,0xa679438e)
-        FF(&b,c,d,a,m[15],22,0x49b40821)
-
-        // Round 2
-        GG(&a,b,c,d,m[1],  5,0xf61e2562)
-        GG(&d,a,b,c,m[6],  9,0xc040b340)
-        GG(&c,d,a,b,m[11],14,0x265e5a51)
-        GG(&b,c,d,a,m[0], 20,0xe9b6c7aa)
-        GG(&a,b,c,d,m[5],  5,0xd62f105d)
-        GG(&d,a,b,c,m[10], 9,0x02441453)
-        GG(&c,d,a,b,m[15],14,0xd8a1e681)
-        GG(&b,c,d,a,m[4], 20,0xe7d3fbc8)
-        GG(&a,b,c,d,m[9],  5,0x21e1cde6)
-        GG(&d,a,b,c,m[14], 9,0xc33707d6)
-        GG(&c,d,a,b,m[3], 14,0xf4d50d87)
-        GG(&b,c,d,a,m[8], 20,0x455a14ed)
-        GG(&a,b,c,d,m[13], 5,0xa9e3e905)
-        GG(&d,a,b,c,m[2],  9,0xfcefa3f8)
-        GG(&c,d,a,b,m[7], 14,0x676f02d9)
-        GG(&b,c,d,a,m[12],20,0x8d2a4c8a)
-
-        // Round 3
-        HH(&a,b,c,d,m[5],  4,0xfffa3942)
-        HH(&d,a,b,c,m[8], 11,0x8771f681)
-        HH(&c,d,a,b,m[11],16,0x6d9d6122)
-        HH(&b,c,d,a,m[14],23,0xfde5380c)
-        HH(&a,b,c,d,m[1],  4,0xa4beea44)
-        HH(&d,a,b,c,m[4], 11,0x4bdecfa9)
-        HH(&c,d,a,b,m[7], 16,0xf6bb4b60)
-        HH(&b,c,d,a,m[10],23,0xbebfbc70)
-        HH(&a,b,c,d,m[13], 4,0x289b7ec6)
-        HH(&d,a,b,c,m[0], 11,0xeaa127fa)
-        HH(&c,d,a,b,m[3], 16,0xd4ef3085)
-        HH(&b,c,d,a,m[6], 23,0x04881d05)
-        HH(&a,b,c,d,m[9],  4,0xd9d4d039)
-        HH(&d,a,b,c,m[12],11,0xe6db99e5)
-        HH(&c,d,a,b,m[15],16,0x1fa27cf8)
-        HH(&b,c,d,a,m[2], 23,0xc4ac5665)
-
-        // Round 4
-        II(&a,b,c,d,m[0],  6,0xf4292244)
-        II(&d,a,b,c,m[7], 10,0x432aff97)
-        II(&c,d,a,b,m[14],15,0xab9423a7)
-        II(&b,c,d,a,m[5], 21,0xfc93a039)
-        II(&a,b,c,d,m[12], 6,0x655b59c3)
-        II(&d,a,b,c,m[3], 10,0x8f0ccc92)
-        II(&c,d,a,b,m[10],15,0xffeff47d)
-        II(&b,c,d,a,m[1], 21,0x85845dd1)
-        II(&a,b,c,d,m[8],  6,0x6fa87e4f)
-        II(&d,a,b,c,m[15],10,0xfe2ce6e0)
-        II(&c,d,a,b,m[6], 15,0xa3014314)
-        II(&b,c,d,a,m[13],21,0x4e0811a1)
-        II(&a,b,c,d,m[4],  6,0xf7537e82)
-        II(&d,a,b,c,m[11],10,0xbd3af235)
-        II(&c,d,a,b,m[2], 15,0x2ad7d2bb)
-        II(&b,c,d,a,m[9], 21,0xeb86d391)
-        
-        state[0].addSafely(a)
-        state[1].addSafely(b)
-        state[2].addSafely(c)
-        state[3].addSafely(d)
     }
  
     public mutating func update(_ d: Data) {
@@ -202,13 +57,141 @@ public struct MD5 {
         }
         transform()
         for i in 0..<4 {
-            let shift = UInt32(i * 8)
-            buffer[i] = UInt8(truncatingIfNeeded: state[0] >> shift)
-            buffer[i + 4] = UInt8(truncatingIfNeeded: state[1] >> shift)
-            buffer[i + 8] = UInt8(truncatingIfNeeded: state[2] >> shift)
-            buffer[i + 12] = UInt8(truncatingIfNeeded: state[3] >> shift)
+            let shift = UInt32(8 * i)
+            for j in 0..<4 {
+                buffer[i + 4 * j] = UInt8(truncatingIfNeeded: state[j] >> shift)
+            }
         }
         return Data(buffer)
     }
     
+    private mutating func transform() {
+        var m = [UInt32](repeating: 0, count: Self.blockSize)
+        var j = 0
+        for i in 0..<Self.blockSize {
+            m[i] = UInt32(data[j]) + (UInt32(data[j + 1]) << 8) + (UInt32(data[j + 2]) << 16) + (UInt32(data[j + 3]) << 24)
+            j += 4
+        }
+        var temp = state
+        
+        func ROTLEFT(_ a: UInt32, _ b: UInt32) -> UInt32 {
+            ((a << b) | (a >> (32 - b)))
+        }
+
+        func F(_ x: UInt32, _ y: UInt32, _ z: UInt32) -> UInt32 {
+            (x & y) | (~x & z)
+        }
+
+        func G(_ x: UInt32, _ y: UInt32, _ z: UInt32) -> UInt32 {
+            (x & z) | (y & ~z)
+        }
+
+        func H(_ x: UInt32, _ y: UInt32, _ z: UInt32) -> UInt32 {
+            x ^ y ^ z
+        }
+
+        func I(_ x: UInt32, _ y: UInt32, _ z: UInt32) -> UInt32 {
+            y ^ (x | ~z)
+        }
+        
+        let functions = [F, G, H, I]
+        var value: UInt32 = 0
+        for i in 0..<functions.count {
+            for item in Self.rounds[i] {
+                temp[item.0].addSafely(functions[i](temp[item.1], temp[item.2], temp[item.3]))
+                temp[item.0].addSafely(m[item.4])
+                temp[item.0].addSafely(item.6)
+                value = temp[item.1]
+                value.addSafely(ROTLEFT(temp[item.0], item.5))
+                temp[item.0] = value
+            }
+        }
+        for i in 0..<state.count {
+            state[i].addSafely(temp[i])
+        }
+    }
+    
+    private var data: [UInt8] = .init(repeating: 0, count: 64)
+    
+    private var dataLength = 0
+    
+    private var bitLength = 0
+    
+    private var state: [UInt32] = [0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476]
+    
+    private static let rounds: [[(Int, Int, Int, Int, Int, UInt32, UInt32)]] = [
+        [
+            (0, 1, 2, 3, 0,  7, 0xd76aa478),
+            (3, 0, 1, 2, 1, 12, 0xe8c7b756),
+            (2, 3, 0, 1, 2, 17, 0x242070db),
+            (1, 2, 3, 0, 3, 22, 0xc1bdceee),
+            (0, 1, 2, 3, 4,  7, 0xf57c0faf),
+            (3, 0, 1, 2, 5, 12, 0x4787c62a),
+            (2, 3, 0, 1, 6, 17, 0xa8304613),
+            (1, 2, 3, 0, 7, 22, 0xfd469501),
+            (0, 1, 2, 3, 8,  7, 0x698098d8),
+            (3, 0, 1, 2, 9, 12, 0x8b44f7af),
+            (2, 3, 0, 1, 10, 17, 0xffff5bb1),
+            (1, 2, 3, 0, 11, 22, 0x895cd7be),
+            (0, 1, 2, 3, 12, 7, 0x6b901122),
+            (3, 0, 1, 2, 13, 12, 0xfd987193),
+            (2, 3, 0, 1, 14, 17, 0xa679438e),
+            (1, 2, 3, 0, 15, 22, 0x49b40821)
+        ],
+        [
+            (0, 1, 2, 3, 1,  5,0xf61e2562),
+            (3, 0, 1, 2, 6,  9,0xc040b340),
+            (2, 3, 0, 1, 11,14,0x265e5a51),
+            (1, 2, 3, 0, 0, 20,0xe9b6c7aa),
+            (0, 1, 2, 3, 5,  5,0xd62f105d),
+            (3, 0, 1, 2, 10, 9,0x02441453),
+            (2, 3, 0, 1, 15,14,0xd8a1e681),
+            (1, 2, 3, 0, 4, 20,0xe7d3fbc8),
+            (0, 1, 2, 3, 9,  5,0x21e1cde6),
+            (3, 0, 1, 2, 14, 9,0xc33707d6),
+            (2, 3, 0, 1, 3, 14,0xf4d50d87),
+            (1, 2, 3, 0, 8, 20,0x455a14ed),
+            (0, 1, 2, 3, 13, 5,0xa9e3e905),
+            (3, 0, 1, 2, 2,  9,0xfcefa3f8),
+            (2, 3, 0, 1, 7, 14,0x676f02d9),
+            (1, 2, 3, 0, 12,20,0x8d2a4c8a)
+        ],
+        [
+            (0, 1, 2, 3, 5,  4,0xfffa3942),
+            (3, 0, 1, 2, 8, 11,0x8771f681),
+            (2, 3, 0, 1, 11,16,0x6d9d6122),
+            (1, 2, 3, 0, 14,23,0xfde5380c),
+            (0, 1, 2, 3, 1,  4,0xa4beea44),
+            (3, 0, 1, 2, 4, 11,0x4bdecfa9),
+            (2, 3, 0, 1, 7, 16,0xf6bb4b60),
+            (1, 2, 3, 0, 10,23,0xbebfbc70),
+            (0, 1, 2, 3, 13, 4,0x289b7ec6),
+            (3, 0, 1, 2, 0, 11,0xeaa127fa),
+            (2, 3, 0, 1, 3, 16,0xd4ef3085),
+            (1, 2, 3, 0, 6, 23,0x04881d05),
+            (0, 1, 2, 3, 9,  4,0xd9d4d039),
+            (3, 0, 1, 2, 12,11,0xe6db99e5),
+            (2, 3, 0, 1, 15,16,0x1fa27cf8),
+            (1, 2, 3, 0, 2, 23,0xc4ac5665)
+        ],
+        [
+            (0, 1, 2, 3, 0,  6,0xf4292244),
+            (3, 0, 1, 2, 7, 10,0x432aff97),
+            (2, 3, 0, 1, 14,15,0xab9423a7),
+            (1, 2, 3, 0, 5, 21,0xfc93a039),
+            (0, 1, 2, 3, 12, 6,0x655b59c3),
+            (3, 0, 1, 2, 3, 10,0x8f0ccc92),
+            (2, 3, 0, 1, 10,15,0xffeff47d),
+            (1, 2, 3, 0, 1, 21,0x85845dd1),
+            (0, 1, 2, 3, 8,  6,0x6fa87e4f),
+            (3, 0, 1, 2, 15,10,0xfe2ce6e0),
+            (2, 3, 0, 1, 6, 15,0xa3014314),
+            (1, 2, 3, 0, 13,21,0x4e0811a1),
+            (0, 1, 2, 3, 4,  6,0xf7537e82),
+            (3, 0, 1, 2, 11,10,0xbd3af235),
+            (2, 3, 0, 1, 2, 15,0x2ad7d2bb),
+            (1, 2, 3, 0, 9, 21,0xeb86d391)
+        ],
+    ]
+
 }
